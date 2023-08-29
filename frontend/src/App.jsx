4056516c85ee75c24c8893b67eec4d262d88c429
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-08-29 19:30:55 -0400

    App.jsx setup with ChakraUI and ReactRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
-function App() {
-  return (
-    <>
-    </>
-  )
-}
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-export default App
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "*",
+    element: <App />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>
+);
