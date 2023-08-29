6862b96925b2cc00c269466b16a88db2ad6f2b4c
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-08-29 19:31:39 -0400

    fixed app.jsx and main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,11 @@
-import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "*",
-    element: <App />
-  }
-]);
+function App() {
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <RouterProvider router={router} />
-    </ChakraProvider>
-  </React.StrictMode>
-);
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "*",
+    element: <App />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>
+);
