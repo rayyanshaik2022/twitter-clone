6ef5405ed4a96f62bde7933aaddc846940b9f8e3
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-08-30 00:09:24 -0400

    Created ui for suggest follow panel

--- frontend/src/components/SuggestFollowPanel.jsx
+++ frontend/src/components/SuggestFollowPanel.jsx
@@ -1,24 +1,60 @@
-import { Flex, VStack, Heading, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  VStack,
+  HStack,
+  Box,
+  Heading,
+  Text,
+  Spacer,
+  Button,
+} from "@chakra-ui/react";
 
 function SuggestFollowPanel() {
   return (
-    <Flex w={"100%"} p={4} bg={"gray.50"} borderRadius={8} gap={8} flexDir={"column"}>
-        <Heading as={"h1"} fontSize={"2xl"} fontWeight={700}>Who to follow</Heading>
+    <Flex
+      w={"100%"}
+      p={4}
+      bg={"gray.50"}
+      borderRadius={8}
+      gap={8}
+      flexDir={"column"}
+    >
+      <Heading as={"h1"} fontSize={"2xl"} fontWeight={700}>
+        Who to follow
+      </Heading>
+      <HStack>
+        <Box boxSize={"40px"} borderRadius={"50%"} bg={"blue.200"} mr={1}></Box>
         <VStack alignItems={"start"} gap={0}>
-            <Text fontSize={"sm"} color={"gray.400"}>Trending</Text>
-            <Heading as={"h2"} fontSize={"lg"}>#monday</Heading>
-            <Text fontSize={"sm"} color={"gray.400"}>12.1k Tweets</Text>
+          <Text fontWeight={"600"}>Display Name</Text>
+          <Text color={"gray.500"}>@Username</Text>
         </VStack>
+        <Spacer />
+        <Button colorScheme="blackAlpha" bg={"black"} borderRadius={"100px"}>
+          Follow
+        </Button>
+      </HStack>
+      <HStack>
+        <Box boxSize={"40px"} borderRadius={"50%"} bg={"blue.200"} mr={1}></Box>
         <VStack alignItems={"start"} gap={0}>
-            <Text fontSize={"sm"} color={"gray.400"}>Trending</Text>
-            <Heading as={"h2"} fontSize={"lg"}>#monday</Heading>
-            <Text fontSize={"sm"} color={"gray.400"}>12.1k Tweets</Text>
+          <Text fontWeight={"600"}>Display Name</Text>
+          <Text color={"gray.500"}>@Username</Text>
         </VStack>
+        <Spacer />
+        <Button colorScheme="blackAlpha" bg={"black"} borderRadius={"100px"}>
+          Follow
+        </Button>
+      </HStack>
+      <HStack>
+        <Box boxSize={"40px"} borderRadius={"50%"} bg={"blue.200"} mr={1}></Box>
         <VStack alignItems={"start"} gap={0}>
-            <Text fontSize={"sm"} color={"gray.400"}>Trending</Text>
-            <Heading as={"h2"} fontSize={"lg"}>#monday</Heading>
-            <Text fontSize={"sm"} color={"gray.400"}>12.1k Tweets</Text>
+          <Text fontWeight={"600"}>Display Name</Text>
+          <Text color={"gray.500"}>@Username</Text>
         </VStack>
+        <Spacer />
+        <Button colorScheme="blackAlpha" bg={"black"} borderRadius={"100px"}>
+          Follow
+        </Button>
+      </HStack>
     </Flex>
   );
 }
