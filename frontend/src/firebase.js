2e93a15bf363a734bd9c44096c032612b634c525
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-08-30 18:44:46 -0400

    initial firebase setup

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, applicationDefault, cert } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
+import { collection, addDoc, getFirestore } from "firebase/firestore"; 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,7 +22,16 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+const useFirestore = () => {
+  return db;
+}
+
 const provider = new GoogleAuthProvider();
 
-export { auth, provider };
+
+
+export { auth, provider, useFirestore };
 
