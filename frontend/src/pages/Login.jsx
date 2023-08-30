f81982bdd356e25f9112905ab433a2710736f18a
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-08-29 20:41:53 -0400

    Added google login to "/log-in" page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,10 +8,40 @@ import {
   Link,
 } from "@chakra-ui/react";
 
+import { auth, provider } from "../firebase";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 import { FaTwitter } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
 function Login() {
+  const onClickLogin = (e) => {
+    e.preventDefault();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...'
+
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+
+        console.log(error);
+      });
+  };
   return (
     <>
       <Flex flexDir={"column"} alignItems={"center"} py={20}>
@@ -33,6 +63,7 @@ function Login() {
             variant={"outline"}
             leftIcon={<FcGoogle />}
             borderRadius={"100px"}
+            onClick={onClickLogin}
           >
             <Center>
               <Text>Sign in with Google</Text>
