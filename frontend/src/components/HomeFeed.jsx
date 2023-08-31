8be031590b5fe309934b25067dc3c413a8096100
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-08-31 14:55:00 -0400

    Query posts

--- frontend/src/components/HomeFeed.jsx
+++ frontend/src/components/HomeFeed.jsx
@@ -1,7 +1,32 @@
 import { Heading } from "@chakra-ui/react";
 import HomePost from "./HomePost";
 
+import { collection, getDocs } from "firebase/firestore";
+import { query, orderBy, limit } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useFirestore } from "../firebase";
+
 function HomeFeed() {
+  const [posts, setPosts] = useState([]);
+  const db = useFirestore();
+
+  useEffect(() => {
+    const myQuery = async () => {
+      const postsRef = collection(db, "Posts");
+      const q = query(postsRef, orderBy("datePosted"), limit(20));
+
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+      });
+
+      console.log(q);
+    };
+
+    myQuery();
+  }, []);
+
   return (
     <>
       <HomePost />
